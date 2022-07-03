42e80a5c6ffc9e4e777b0cfd59a893e594feb55b
Kobi Hari <kobi2294@yahoo.com>
2022-07-03 16:11:10 +0300

    end of promises

--- Module 03/fun-with-promises/src/app/app.component.ts
+++ Module 03/fun-with-promises/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { compileNgModule } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { combineLatestInit } from 'rxjs/internal/observable/combineLatest';
 
 @Component({
   selector: 'app-root',
@@ -33,7 +34,7 @@ export class AppComponent {
   async start() {
     console.log('1');
 
-    let p = this.createPromise();
+    let p = this.createRandomInProgressPromise();
     console.log('2');
 
     let res = await p;
@@ -42,6 +43,53 @@ export class AppComponent {
 
     console.log(`Promise completed with result ${res}`);
     return true;
+  }
+
+  async go2() {
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let promises = arr.map(i => this.createRandomInProgressPromise());
+
+    console.log('All promises are running');
+
+    let pAll = Promise.all(promises);
+    let resAll = await pAll;
+
+    let pAny = Promise.race(promises);
+
+    console.log(`all promises completed, their results are ${resAll}`);
+
+  }
+
+  createCompletedPromoise() {
+    return Promise.resolve(10);
+  }
+
+  createFaultedPromise() {
+    return Promise.reject<number>('Oy basa');
+  }
+
+  createRandomInProgressPromise(): Promise<number> {
+    let delay = Math.random() * 4000 + 1000; // delay between 1000 and 5000
+    let res = Math.ceil(Math.random() * 20 + 40); // result between 40 and 60
+
+    return new Promise((success, failure) => {
+      setTimeout(() => success(res), delay);
+    });
+  }
+
+  protected isPromiseInProgress: boolean = false;
+  protected promiseResolver: (n: number) => void = _ => {}
+
+  createPromiseThatEndsWhenTheUserClicksTheButton(): Promise<number> {
+    this.isPromiseInProgress = true;
+    return new Promise<number>((resolve, reject) => {
+      this.promiseResolver = resolve;
+    });
+  }
+
+  completeCurrentPromise(result: number) {
+    this.promiseResolver(result);
+    this.promiseResolver = _ => {};
   }
 
 }
